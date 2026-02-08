4e8761823c60bc026e1a99237d6da0070996449f
Paul Velasquez <paul.velasquez@swiftly-workspace.com>
2026-02-08 22:20:29 +0800

    chore: Remove JS and RN SDKs in preparation for subtree migration

--- SwiftlyFeedbackKit-RN/jest.setup.js
+++
@@ -1,4 +0,0 @@
-// Mock AsyncStorage
-jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
-);
--- SwiftlyFeedbackKit-RN/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * FeedbackKit React Native Components
- */
-
-export { StatusBadge, type StatusBadgeProps } from './StatusBadge';
-export { CategoryBadge, type CategoryBadgeProps } from './CategoryBadge';
-export { VoteButton, type VoteButtonProps } from './VoteButton';
-export { FeedbackCard, type FeedbackCardProps } from './FeedbackCard';
-export { FeedbackList, type FeedbackListProps } from './FeedbackList';
